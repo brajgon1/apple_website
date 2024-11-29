9e1a186ad9247dc1e3ac3741becb32fd836c5075
Brandon Gonzalez <brajgon@gmail.com>
2024-11-28 20:29:28 -0700

    navlists

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import { appleImg, bagImg, searchImg } from "../utils";
+import { navLists } from "../constants";
 
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" width={14} height={18}/>
-        <div>
-            {['Phones', 'Macbooks', 'Tablets'].map((nav) => (
+        <div className="flex flex-1 justify-center max-sm:hidden">
+            {navLists.map((nav) => (
                 <div key={nav}>
                     {nav}
                 </div>
