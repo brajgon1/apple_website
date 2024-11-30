282b1fcbc9da13c47c0310c00be3d85b88d0eea7
Brandon Gonzalez <brajgon@gmail.com>
2024-11-30 00:34:31 -0700

    imported useEffect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,28 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { heroVideo, smallHeroVideo } from "../utils";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Hero = () => {
-  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
 
   useGSAP(() => {
     gsap.to("#hero", {
@@ -20,11 +38,20 @@ const Hero = () => {
           iPhone 15 Pro
         </p>
         <div className="md:w-10/12 w-9/12">
-          <video autoPlay muted playsInline={true} key={videoSrc}>
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
       </div>
+
+      <div></div>
+
     </section>
   );
 };
